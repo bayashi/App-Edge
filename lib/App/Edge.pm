db12a972c21ed280ba9d001536524d3c95eb55de
bayashi <bayashi@cpan.org>
2014-06-23 23:29:15 +0900

    initial commit

---
+++ lib/App/Edge.pm
@@ -0,0 +1,56 @@
+package App::Edge;
+use strict;
+use warnings;
+use Carp qw/croak/;
+
+our $VERSION = '0.01';
+
+sub new {
+    my $class = shift;
+    my $args  = shift || +{};
+
+    bless $args, $class;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+App::Edge - one line description
+
+
+=head1 SYNOPSIS
+
+    use App::Edge;
+
+
+=head1 DESCRIPTION
+
+App::Edge is
+
+
+=head1 REPOSITORY
+
+App::Edge is hosted on github: L<http://github.com/bayashi/App-Edge>
+
+Welcome your patches and issues :D
+
+
+=head1 AUTHOR
+
+Dai Okabayashi E<lt>bayashi@cpan.orgE<gt>
+
+
+=head1 SEE ALSO
+
+L<Other::Module>
+
+
+=head1 LICENSE
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
+
+=cut
---
+++ t/01_basic.t
@@ -0,0 +1,11 @@
+use strict;
+use warnings;
+use Test::More;
+
+use App::Edge;
+
+can_ok 'App::Edge', qw/new/;
+
+# write more tests
+
+done_testing;
